6a13056d660a272e27200afdd4ca009d2789ee72
Somil <somilshivhare2099@gmail.com>
2025-12-20 19:02:45 +0530

    Update CORS to support production URLs via environment variable

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -32,7 +32,7 @@ await connectDB();
 // setup socket.io
 const io = new IoServer(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
